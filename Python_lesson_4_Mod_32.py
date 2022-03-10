298b3eab1245c413d94d3280edefe56807829657
g-ung <45480999+g-ung@users.noreply.github.com>
2022-03-10 22:56:20 +1100

    supplement to comments

--- Python_lesson_4_Mod_32.py
+++ Python_lesson_4_Mod_32.py
@@ -5,10 +5,12 @@ check_prime = [26, 39, 51, 53, 57, 79, 85]
 ## write your code here
 ## HINT: You can use the modulo operator to find a factor
 for num in check_prime: # iterate through the check_prime list
-    for index in range(2, num): # search for factors, iterating through range starting from 2 num-1, i.e. for num = 26 check for factors up to 26
+    # search for factors, iterating through range starting from 2 num-1, i.e. for num = 26 check for factors up to 26
+    for index in range(2, num): 
         if (num % index) == 0: # prime number check, if mod is 0
             print("{} is NOT a prime number because {} is a factor of {}".format(num, index, num))
             break
-        elif index == num -1: # keep checking until all possible factors if prime found print is a prime message
+        # keep checking until all possible factors if prime found print is a prime message if we iterate through to num-1 and haven't break out of loop then it is a prime
+        elif index == num -1: 
             print("{} IS a prime number".format(num))
         
