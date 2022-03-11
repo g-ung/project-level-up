edd4fd5c4afd8e7b05421dd15759ce8303771d0f
g-ung <45480999+g-ung@users.noreply.github.com>
2022-03-11 12:00:55 +1100

    Update Python_lesson_4_Mod_34_zip_coord.py
    add zip list to dictionary

--- Python_lesson_4_Mod_34_zip_coord.py
+++ Python_lesson_4_Mod_34_zip_coord.py
@@ -1,4 +1,4 @@
-# AI Porgamming with Python: Lesson 4 Control Flow, Module 34: Zip Coordinates and Zip to list a dictionary
+# AI Porgamming with Python: Lesson 4 Control Flow, Module 34: Zip Coordinates; Zip Lists a Dictionary; Unzip Tuples
 ## Zip Coordinates
 x_coord = [23, 53, 2, -12, 95, 103, 14, -5]
 y_coord = [677, 233, 405, 433, 905, 376, 432, 445]
@@ -13,9 +13,18 @@ for point in zip(labels, x_coord, y_coord, z_coord): # point is the loop variabl
 for point in points: # iterate through unpacked list of tuples from first for loop where points is mapped points list of tuples
     print(point)
 
-## Zip to list a dictionary
+## zip to list a dictionary
 cast_names = ["Barney", "Robin", "Ted", "Lily", "Marshall"]
 cast_heights = [72, 68, 72, 66, 76]
 
 cast = dict(zip(cast_names, cast_heights))
 print("\n", cast)
+
+## zip list to a dictionary
+cast = (("Barney", 72), ("Robin", 68), ("Ted", 72), ("Lily", 66), ("Marshall", 76))
+
+# define names and heights here
+names, heights = zip(*cast)
+
+print("\n", names)
+print(heights)
