2b052f6288634e74f5d6f19e4d17863a06b599c1
g-ung <45480999+g-ung@users.noreply.github.com>
2022-03-22 12:54:26 +1100

    list comprehension solution w for loop version for practice

--- Python_lesson_6_mod_18.py
+++ Python_lesson_6_mod_18.py
@@ -5,14 +5,17 @@ def create_cast_list(filename):
     cast_list = []
     #use with to open the file filename
     with open('/Users/gabe_ung/Desktop/flying_circus_cast.txt') as f:
-        contents = f.readlines()
-        print(contents)
     #use the for loop syntax to process each line
-        for line in contents:
-           line.split()
-           print(line)
+    ## list comprehension
+        cast_list = [line.split(",")[0] for line in f]
+        
+    ## for loop version for practice
+        '''
+        for line in f:
+            contents = line.split(",")[0]        
     #and add the actor name to cast_list
-
+            cast_list.append(contents)
+        '''
     return cast_list
 
 cast_list = create_cast_list('flying_circus_cast.txt')
