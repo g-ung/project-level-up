386adfb527fa15395b893f9e4bfb3b76879b87a2
g-ung <45480999+g-ung@users.noreply.github.com>
2022-03-23 15:18:09 +1100

    add lesson 6 quiz from mod 31

---
+++ Python_lesson_6_mod_31.py
@@ -0,0 +1,35 @@
+# AI Porgamming with Python: Lesson 6 Scripting, Module 31: Practice Question
+
+# Create a function that opens the flowers.txt, reads every line in it, and saves it as a dictionary. 
+# The main (separate) function should take user input (user's first name and last name) and parse the 
+# user input to identify the first letter of the first name. It should then use it to print the flower 
+# name with the same first letter (from dictionary created in the first function).
+
+#  Write your code here
+
+# HINT: create a dictionary from flowers.txt
+
+# HINT: create a function to ask for user's first and last name
+# function: builds flower_dict from file: flowers.txt
+def name_flower_dict(filename):
+    flower_dict = {}
+    with open('/Users/gabe_ung/Desktop/flowers.txt', 'r') as f: # open file: flowers.txt
+        for line in f:
+            letter, flower = line.strip().split(':') # split the line where there is a ':'
+            flower_dict[letter] = flower.strip() # build dictionary with key (letter), value (flower) from file: flowers.txt
+    return flower_dict
+
+# main function: prompts user for input, parses the first letter of their name
+# this function also calls name_flower_dict function to create the dictionary
+def main():
+    the_flower_dict = name_flower_dict('/Users/gabe_ung/Desktop/flowers.txt')
+    full_name = input("Please enter your first [space] last name only: ")
+    first_name = full_name[0]
+    first_letter = first_name[0]
+    # prints output from user with value for corresponding key in flower_dict dictionary
+    print("Unique flower name with the first letter: {}".format(the_flower_dict[first_letter]))
+main()
+
+# print the desired output
+# check with print
+# print(flower_dict)
