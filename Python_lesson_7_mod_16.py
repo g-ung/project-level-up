2f361427bffa1fa67eae3642a6e5d7710e6d9059
g-ung <45480999+g-ung@users.noreply.github.com>
2022-03-28 22:06:52 +1100

    add Cothing class with new methods

---
+++ Python_lesson_7_mod_16.py
@@ -0,0 +1,72 @@
+# AI Porgamming with Python: Lesson 7 Object-Oriented Programming, Module 16 Inheritace
+
+class Clothing:
+
+    def __init__(self, color, size, style, price):
+        self.color = color
+        self.size = size
+        self.style = style
+        self.price = price
+        
+    def change_price(self, price):
+        self.price = price
+        
+    def calculate_discount(self, discount):
+        return self.price * (1 - discount)
+    
+    def calculate_shipping(self, weight, rate):
+        '''
+        Method to calculate the shipping price
+
+        Args:
+            weight (float) weight of the article
+            rate (float) shipping weight
+
+        returns:
+            the shipping price (float): weigth * rate
+        '''
+        return weight * rate
+        
+class Shirt(Clothing):
+    
+    def __init__(self, color, size, style, price, long_or_short):
+        
+        Clothing.__init__(self, color, size, style, price)
+        self.long_or_short = long_or_short
+    
+    def double_price(self):
+        self.price = 2 * self.price
+    
+class Pants(Clothing):
+
+    def __init__(self, color, size, style, price, waist):
+        
+        Clothing.__init__(self, color, size, style, price)
+        self.waist = waist
+        
+    def calculate_discount(self, discount):
+        return self.price * (1 - discount / 2)
+    
+# TODO: Write a class called Blouse, that inherits from the Clothing class
+# and has the the following attributes and methods:
+#   attributes: color, size, style, price, country_of_origin
+#     where country_of_origin is a string that holds the name of a 
+#     country
+#
+#   methods: triple_price, which has no inputs and returns three times
+#     the price of the blouse
+#
+class Blouse(Clothing):
+    def __init__(self, color, size, style, price, country_of_origin):
+        
+        Clothing.__init__(self, color, size, style, price)
+        self.country_of_origin = country
+    
+    def triple_price(self):
+        return self.price * 3
+
+# TODO: Add a method to the clothing class called calculate_shipping.
+#   The method has two inputs: weight and rate. Weight is a float
+#   representing the weight of the article of clothing. Rate is a float
+#   representing the shipping weight. The method returns weight * rate
+    
