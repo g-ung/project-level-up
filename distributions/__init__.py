a6f19df9e52384bd4b5b060c97e6bff565601bab
g-ung <45480999+g-ung@users.noreply.github.com>
2022-04-04 12:08:52 +1000

    Moved __init__ file to distribution folder and added comments to __init__

--- distributions/__init__.py
+++ distributions/__init__.py
@@ -1,2 +1,9 @@
+'''  
+This __init__ file imports the Gaussian class from the Guassiandistribution module
+and the Binomial class from the Binomialdistribution module, i.e. we are importing
+Guassian and Binomial class directly.
+'''
+
 from .Gaussiandistribution import Gaussian
 from .Binomialdistribution import Binomial
+
