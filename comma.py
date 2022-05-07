661e22a0567ded60499506bb319fa6a356190b6f
g-ung <45480999+g-ung@users.noreply.github.com>
2022-05-07 15:11:50 +1000

    add context

--- comma.py
+++ comma.py
@@ -1,3 +1,13 @@
+'''
+my_list = ['apples', 'bananas', 'tofu', 'cats']
+
+Write a function that takes a list value as an argument and returns a string 
+with all the items separated by a comma and a space, with and inserted before 
+the last item. For example, passing the previous spam list to the function 
+would return 'apples, bananas, tofu, and cats'. But your function should be 
+able to work with any list value passed to it. Be sure to test the case where 
+an empty list [] is passed to your function.
+'''
 def comma(some_list):
   # for loop, string.find() approach
   some_list.insert(3, 'and')
@@ -12,6 +22,6 @@ def comma(some_list):
   return print(string_final)
   
 my_list = ['apples', 'bananas', 'tofu', 'cats']
-#my_list = []
+# my_list = []
 comma(my_list)
 
