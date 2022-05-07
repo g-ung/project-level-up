b4af10827de42685f0b6fd6bbbc4180ae354749f
g-ung <45480999+g-ung@users.noreply.github.com>
2022-05-07 15:05:00 +1000

    recoded comma function to be more general

--- comma.py
+++ comma.py
@@ -1,22 +1,17 @@
 def comma(some_list):
-  # join() list, slice approach
-  some_list.insert(3, 'and')  
-  string_final = " ".join(some_list)
-  string_final = string_final[:19] + ',' + string_final[19:-1]
-
-  return print(string_final)
-
-def comma_one(some_list):
   # for loop, string.find() approach
+  some_list.insert(3, 'and')
+  some_list.insert(3, ',')
   string = ""
   for i in some_list:
     string += i + " "
   
-  index = string.find('cats')
-  string_final = string[:index-1] + ', and ' + string[index:]
+  index = string.find(',')
+  string_final = string[:index-1] + '' + string[index:]
 
   return print(string_final)
   
 my_list = ['apples', 'bananas', 'tofu', 'cats']
-comma_one(my_list)
+#my_list = []
+comma(my_list)
 
