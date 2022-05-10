88a0c0d9107652cdbb04ece7a70fc0f69f5ecfe6
g-ung <45480999+g-ung@users.noreply.github.com>
2022-05-10 15:30:11 +1000

    Add annotation to code

--- machine_learning/coin_flip_streaks.py
+++ machine_learning/coin_flip_streaks.py
@@ -2,25 +2,38 @@ import random
 
 num_of_streaks = 0
 streak_count = 0
-heads = 0
-tails = 0
 flip_list = []
 
 for sample_size in range(10000):
     # Code that creates a list of 100 'heads or 'tails' values.
     for i in range(100):
-        if random.randint(0,1) == 1:
+        if random.randint(0,1) == 1: # if 1 it is heads, append() 'H' to some_list
             flip_list.append('H')
-            heads += 1
-        if heads == 6:
-            streak_count += 1
-    else:
-        flip_list.append('T')
-        tails += 1
-        if tails == 6:
-            streak_count += 1
+        else: # if 0 it is tails, append() 'T' to some_list
+            flip_list.append('T')
 # Code that checks if there is a streak of 6 heads or tails in a row.
-    if streak_count == 6:
-        num_of_streaks += 1
+    for index, item in enumerate(flip_list):
+        '''
+        Could use for i in range(len(some_list)) to access the index in loop
+        variable i enumerate returns an iterable with index
+        REFERENCE:
+        https://www.programiz.com/python-programming/methods/built-in/enumerate
+        '''
+        if index == 0: # in postion 0 do nothing
+            pass
+        elif flip_list[index] == flip_list[index-1]:
+            '''
+            This block of code checks if preceeding item with proceeding item in 
+            list value are equal, i.e. the same either heads or tails.  For each 
+            matching item streak_count is incremented by 1, 6 streak_counts 
+            counts as 1 num_of_streaks, i.e. total streak count
+            '''
+            streak_count += 1
+        else: # if no match found steak_count is set to 0
+            streak_count = 0
+        
+        if streak_count == 6: # when a streak_count == 6 num_of_streaks is incremented by 1
+            num_of_streaks += 1
+            break
   
-print('Chance of streak: %s%%' % (num_of_streaks / 100))
+print("Chance of streak: {}%".format(num_of_streaks / 100))
