2ffdfb047f2e3d31d0ec86f6f4163a17c6fb5e0d
g-ung <45480999+g-ung@users.noreply.github.com>
2022-05-11 14:27:30 +1000

    add tic tac toe file

---
+++ tic-tac-toe_model_with_dict.py
@@ -0,0 +1,28 @@
+# exercise for demonstration excersie to use dictionary to model a naughts and crosses game (not complete game)
+def print_board(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+print_board(the_board)
+
+# use dictionary to represent a naughts and crosses, aka tic-tac-toe, board
+# the data strucutre stored in the_board variable represents the naughts 
+# and crosses board
+the_board = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+            'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+            'low-L': ' ', 'low-M': ' ', 'low_R': ' '}
+
+turn = "X" # represent the value to the dictionary keys
+for i in range(9):
+    print_board(the_board)
+    print("Turn for {}. Move on which space?".format(turn))
+    move = input() # prompt player to enter their move
+    the_board[move] = turn # updates the board with player's move accordingly, note variable move represents dictionary keys
+    if turn == "X": # swap player before moving to next turn
+        turn = "O"
+    else:
+        turn = "X"
+
+print_board(the_board)
