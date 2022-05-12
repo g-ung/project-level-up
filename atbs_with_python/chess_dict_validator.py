aad0aac18cac9aea288b1fd53104a94bff9f884f
g-ung <45480999+g-ung@users.noreply.github.com>
2022-05-12 22:33:24 +1000

    Add code to build chess board

--- atbs_with_python/chess_dict_validator.py
+++ atbs_with_python/chess_dict_validator.py
@@ -1,18 +1,38 @@
+'''
+REQUIREMENTS:
+1. Valid board must have exactly one black king and exactly one white king
+2. Each player can only have at most 16 pieces, 8 pawns, and all pieces must
+be on valid tile from '1a' to '8h'
+3 Pieces name begine with a 'w' or 'b' to represent white or black followed
+by name of the pieces, i.e. 'pawn', 'knight', 'bishop' etc. eg. 'wpawn'
+4. The funtion should detect when a bug has reulsted in an impropter chess board 
+'''
 
-def is_valid_chess_board(board):
+def is_valid_chess_board():
+    # create empty chess board to store key: coordinates, value: chess pieces
+    chess_board_dict = {}
+
+    # Build the chess board
+    chess_board_list = []
+    for num in range(len(board_yaxis)):
+        for char in range(len(board_xaxis)):
+            chess_board_list.append(char + num)
+
 
 # chess board represented in a dictionary
-chess_board = {'1a': ' ', '1b': ' ', '1c': ' ', '1d': ' ', '1e': ' ', '1f': ' ', '1g': ' ', '1h': ' ',
-               '2a': ' ', '2b': ' ', '2c': ' ', '2d': ' ', '2e': ' ', '2f': ' ', '2g': ' ', '2h': ' ',
-               '3a': ' ', '3b': ' ', '3c': ' ', '3d': ' ', '3e': ' ', '3f': ' ', '3g': ' ', '3h': ' ',
-               '4a': ' ', '4b': ' ', '4c': ' ', '4d': ' ', '4e': ' ', '4f': ' ', '4g': ' ', '4h': ' ',
-               '5a': ' ', '5b': ' ', '5c': ' ', '5d': ' ', '5e': ' ', '5f': ' ', '5g': ' ', '5h': ' ',
-               '6a': ' ', '6b': ' ', '6c': ' ', '6d': ' ', '6e': ' ', '6f': ' ', '6g': ' ', '6h': ' ',
-               '7a': ' ', '7b': ' ', '7c': ' ', '7d': ' ', '7e': ' ', '7f': ' ', '7g': ' ', '7h': ' ',
-               '8a': ' ', '8b': ' ', '8c': ' ', '8d': ' ', '8e': ' ', '8f': ' ', '8g': ' ', '8h': ' '}
+chess_board = {'a1': ' ', 'b1': ' ', 'c1': ' ', 'd1': ' ', 'e1': ' ', 'f1': ' ', 'g1': ' ', 'h1': ' ',
+               'a2': ' ', 'b2': ' ', 'c2': ' ', 'd2': ' ', 'e2': ' ', 'f2': ' ', 'g2': ' ', 'h2': ' ',
+               'a3': ' ', 'b3': ' ', 'c3': ' ', 'd3': ' ', 'e3': ' ', 'f3': ' ', 'g3': ' ', 'h3': ' ',
+               'a4': ' ', 'b4': ' ', 'c4': ' ', 'd4': ' ', 'e4': ' ', 'f4': ' ', 'g4': ' ', 'h4': ' ',
+               'a5': ' ', 'b5': ' ', 'c5': ' ', 'd5': ' ', 'e5': ' ', 'f5': ' ', 'g5': ' ', 'h5': ' ',
+               'a6': ' ', 'b6': ' ', 'c6': ' ', 'd6': ' ', 'e6': ' ', 'f6': ' ', 'g6': ' ', 'h6': ' ',
+               'a7': ' ', 'b7': ' ', 'c7': ' ', 'd7': ' ', 'e7': ' ', 'f7': ' ', 'g7': ' ', 'h7': ' ',
+               'a8': ' ', 'b8': ' ', 'c8': ' ', 'd8': ' ', 'e8': ' ', 'f8': ' ', 'g8': ' ', 'h8': ' '}
                
 # building the chess board from it's constituent parts
-board_xaxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-board_yaxis = [1, 2, 3, 4, 5, 6, 7, 8]
-pieces = ['paw', 'rook', 'knight', 'bishop', 'queen', 'king']
-piece_colour = ['b', 'w']
+# coordinates that represent the chess board, 64 tiles
+board_xaxis = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h')
+board_yaxis = (1, 2, 3, 4, 5, 6, 7, 8)
+# all possible peices, 61 pieces
+pieces = ('pawn', 'rook', 'knight', 'bishop', 'queen', 'king')
+piece_colour = ('b', 'w')
