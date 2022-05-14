f667f4c9cebe0ab53155b10f3dfc28c286cb1b91
g-ung <45480999+g-ung@users.noreply.github.com>
2022-05-14 22:45:33 +1000

    coded chess_board_dict build
    WIP: coded chess_board_dict build from 2 different length list, kinda works

--- atbs_with_python/chess_dict_validator.py
+++ atbs_with_python/chess_dict_validator.py
@@ -27,8 +27,8 @@ def is_valid_chess_board():
             '''
             chess_board_list.append(chr(char) + str(num))
             # use get() to build chess_board_dict, i.e. add chess_ board_list to chess_board_dict as dict. key
-            for tile_coord in chess_board_list:
-                chess_board_dict[tile_coord] = chess_board_dict.get(tile_coord, ' ')
+            #for tile_coord in chess_board_list:
+                #chess_board_dict[tile_coord] = chess_board_dict.get(tile_coord, ' ')
     
     # build chess pieces by marryiing chess piece colours to chess piece set
     colourcoded_pieces = [] # empty list to stored the completed chess pieces (with colours)
@@ -61,10 +61,10 @@ def is_valid_chess_board():
                         colourcoded_pieces.append('w' + item + str(i)) 
 
    # adding chess pieces to chess_board_dict
-   
+    chess_board_dict = {x: colourcoded_pieces[i % len(colourcoded_pieces)] for i, x in enumerate(chess_board_list)} # this kinda works.....
    # return print(colourcoded_pieces)
    # return print(chess_board_list)
-   # return print(chess_board_dict)
+    return print(chess_board_dict)
 
 '''
 # chess board represented in a dictionary  
