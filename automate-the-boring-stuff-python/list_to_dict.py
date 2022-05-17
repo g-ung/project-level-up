69ce8cbef2db3e704558c81fe0037c7eabfcbde6
g-ung <45480999+g-ung@users.noreply.github.com>
2022-05-17 23:19:26 +1000

    Corrected typos in comments

--- automate-the-boring-stuff-python/list_to_dict.py
+++ automate-the-boring-stuff-python/list_to_dict.py
@@ -1,6 +1,6 @@
 '''
 List to Dictionary Function for Fantasy Game Inventory
-REQUIREMMENTS:
+REQUIREMENTS:
 1. Write a function named add_to_inventory(inventory, added_items), where the "inventory"
 parameter is the dictionary representing the player's inventory and the "added_items" 
 parameter is the list like dragon_loot
@@ -16,23 +16,23 @@ Inventory:
 Total number of items: 48
 
 NOTE: The added_items list can contain multiples of the same item.
-NOTE: You will need to include the function you wrote for the previous 
+You will need to include the function you wrote for the previous 
 program: fantasy_game_inventory.py in this script
 
 '''
 def add_to_inventory(inventory, added_items):
-    for item in added_items: # dragon_loot argument parsed in as a parameter to add_items then iterate through the list
+    for item in added_items: # dragon_loot argument parsed as a parameter to add_items then iterate through the list
         '''
-        If a key does not yet exist the inventory dictionary, arguement current_inv pasrsed as parameter
-        to inventory, get() will return value 0 and inventory[item] will be set to 1. When the iteration
+        If a key does not yet exist the inventory dictionary, arguement current_inv parsed as parameter
+        to inventory, get() will return value 0 and inventory[item] is set to 1. When the iteration
         encounters another item that already exist in the dictionary the value of the key is 1++, i.e.
-        the 2nd encounter of 'gold coin' key the value would be 1++ resulting in 2
+        the 2nd encounter of 'gold coin' key the value would be 1++ resulting in a count of 2
         REFERENCE:
         https://www.programiz.com/python-programming/methods/dictionary/get
         '''
         inventory[item] = inventory.get(item, 0) + 1
     
-    return inventory # use explicity return statement to return inventory so it can be parsed into the function add_to_inventory
+    return inventory # use explicity return statement to return inventory so it can be parsed to the function add_to_inventory
 
 def display_inventory(inventory):
     item_count = 0 # variable to store total number of items in iventory, i.e. the values in the dictionary
