74f76f43c8788dcedbe5e93da9003695d26f1afe
g-ung <45480999+g-ung@users.noreply.github.com>
2022-05-17 19:20:06 +1000

    add 2 new project files

---
+++ automate-the-boring-stuff-python/fantasy_game_inventory.py
@@ -0,0 +1,26 @@
+'''
+REQUIREMENTS:
+1. Write a function named display_Inventory() that would take any possible "inventory"
+and display it like the following:
+Inventory:
+12 arrow
+42 gold coin
+1 rope
+6 torch
+1 dagger
+Total number of items: 62
+2. Write a function named add_to_inventory(inventory, added_items), where the "inventory"
+parameter is the dictionary representing the player's inventory and the "added_items" 
+parameter is the list like dragon_loot
+3. the add_to_inventory() function should return a dictionary that represents the updated
+inventory
+
+NOTE: The added_items list can contain multiples of the same item.
+'''
+def display_inventory():
+
+def add_to_inventory(inventory, added_items):
+
+current_inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+
---
+++ automate-the-boring-stuff-python/list_to_dict.py
@@ -0,0 +1,21 @@
+'''
+List to Dictionary Function for Fantasy Game Inventory
+REQUIREMMENTS:
+1. Write a function named add_to_inventory(inventory, added_items), where the "inventory"
+parameter is the dictionary representing the player's inventory and the "added_items" 
+parameter is the list like dragon_loot
+2. the add_to_inventory() function should return a dictionary that represents the updated
+inventory
+3. Expected output:
+
+Inventory:
+
+45 gold coin
+1 rope
+1 ruby
+1 dagger
+Total number of items: 48
+
+NOTE: The added_items list can contain multiples of the same item.
+'''
+def add_to_inventory(inventory, added_items):
