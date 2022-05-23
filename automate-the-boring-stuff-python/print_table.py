a579302f34b132b73ce192e8305a8a1c3406d20c
g-ung <45480999+g-ung@users.noreply.github.com>
2022-05-23 22:14:24 +1000

    delete for loop
    Starting again

--- automate-the-boring-stuff-python/print_table.py
+++ automate-the-boring-stuff-python/print_table.py
@@ -10,22 +10,10 @@ REQUIREMENTS:
 cherries Carol moose
   banana David goose
 
-NOTE: Assum that all the innner lists will contain the same number of strings
+NOTE: Assume that all the innner lists will contain the same number of strings
 '''
 def print_table(table):
     col_widths = [0] * len(table) # create a list containing the same no. of 0 values as there are no. of inner lists in tabel_data
-    
-    for i, list in enumerate(table): 
-        '''
-        Iterate through table with enumerate to access the nested lists to find the longest string 
-        and store that value to col_width[index]
-        '''
-        for string in list:
-            longest_string = ''
-            if longest_string > col_widths[i]:
-                col_widths = longest_string 
-
-    print(col_width)
 
 table_data = [['apples', 'oranges', 'cherries', 'banana'], 
             ['Alice', 'Bob', 'Carol', 'David'],
