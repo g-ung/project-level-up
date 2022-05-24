9b45b043bf41a31b510b565c8c16468662879586
g-ung <45480999+g-ung@users.noreply.github.com>
2022-05-24 19:52:13 +1000

    Use zip and transpose approach

--- automate-the-boring-stuff-python/print_table.py
+++ automate-the-boring-stuff-python/print_table.py
@@ -13,7 +13,20 @@ cherries Carol moose
 NOTE: Assume that all the innner lists will contain the same number of strings
 '''
 def print_table(table):
-    col_widths = [0] * len(table) # create a list containing the same no. of 0 values as there are no. of inner lists in tabel_data
+    col_widths = [0] * len(table) # create a list containing the same no. of 0 values as there are no. of inner lists in table_data
+
+    transpose = list(zip(*table))
+    transpose = [list(sublist) for sublist in transpose]
+    transpose = [item for sublist in transpose for item in sublist]
+
+    list_to_string = ' '.join(transpose)
+    print(list_to_string)
+    #print(transpose[:3], transpose[3:6], transpose[6:9], transpose[9:])
+
+
+   
+    # check with
+    # print(transpose)
 
 table_data = [['apples', 'oranges', 'cherries', 'banana'], 
             ['Alice', 'Bob', 'Carol', 'David'],
