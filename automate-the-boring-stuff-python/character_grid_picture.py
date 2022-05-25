003e19f8405e3ac0f3e55902e3dd00bf4b5304a3
g-ung <45480999+g-ung@users.noreply.github.com>
2022-05-25 14:29:42 +1000

    add character grid picture file

---
+++ automate-the-boring-stuff-python/character_grid_picture.py
@@ -0,0 +1,31 @@
+'''
+Character Picture Grid
+REQUIREMENTS:
+1. Convert the grid value of 'grid' to print the image:
+
+..OO.OO..
+.OOOOOOO.
+.OOOOOOO.
+..OOOOO..
+...OOO...
+....O....
+
+2. Use a loop in a loop to print grid[0][0], then grid[1][0], then grid[2]
+[0], and so on, up to grid[8][0] to prin the first row, then print a newline
+3. Then, pring grid[0][1], then grid[1][1], then grid[2][2], and so on. The
+last thing your program will print is grid[8][5]
+
+NOTE: Remember to pass the end=' ' keyword arguement to print() if you don't want
+a newline printed automatically after each print() call
+'''
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+
