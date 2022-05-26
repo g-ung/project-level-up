cd9bde72bd8ee547e5d05802d5ef72eca6ccc76e
g-ung <45480999+g-ung@users.noreply.github.com>
2022-05-26 22:00:19 +1000

    No changes

--- automate-the-boring-stuff-python/print_table.py
+++ automate-the-boring-stuff-python/print_table.py
@@ -13,14 +13,13 @@ cherries Carol moose
 NOTE: Assume that all the innner lists will contain the same number of strings
 '''
 def print_table(table):
-    
     # transpose the nested list and print the table
     for i in zip(*table):
         transposed_list = list(i) # list constructor to i, y-axis of transposed table list, to new transposed list
         #print(transposed_list)
         #print(max(transposed_list, key=len))
 
-        print(transposed_list[0].rjust(8), transposed_list[1].rjust(5), transposed_list[2].rjust(5)) # use index to print transposed_listd
+        print(transposed_list[0].rjust(8), transposed_list[1].rjust(5), transposed_list[2].rjust(5)) # use index to print each columm transposed_list
 
 table_data = [['apples', 'oranges', 'cherries', 'banana'],
               ['Alice', 'Bob', 'Carol', 'David'],
