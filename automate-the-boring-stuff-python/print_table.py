2de7217a16295cb4e02daade59cdef2c199cd08c
g-ung <45480999+g-ung@users.noreply.github.com>
2022-05-26 19:22:14 +1000

    Removed loop code to find longest string

--- automate-the-boring-stuff-python/print_table.py
+++ automate-the-boring-stuff-python/print_table.py
@@ -13,32 +13,14 @@ cherries Carol moose
 NOTE: Assume that all the innner lists will contain the same number of strings
 '''
 def print_table(table):
-    col_widths = [0] * len(table) # create a list containing the same no. of 0 values as there are no. of inner lists in table_data
-    '''
-    # find the longest string in nested list table (table_data)
-  
-    for x_axis in range(len(table)): # iterate through len() of table to access the index, i.e. 0, 1, 2
-        # check with
-        # print(i)
-        for y_axis in table[x_axis]: # iterate through the first list, index 0, of table to access the list in position 0
-            # check with
-            # print(j)
-            
-            Compare the length of the item (string) in col_widths in index x_axis with 
-            the item (string) in the first list in table.  If col_widths[x_axis] < len(y_axis)
-            update with len(y_axis) as longest string
-            
-            if col_widths[x_axis] < len(y_axis): 
-                col_widths[x_axis] = len(y_axis)
-                # check with
-                # print(col_widths[x_axis])
-    '''
+    
     # transpose the nested list and print the table
     for i in zip(*table):
-        # print(i)
-        transposed_list = list(i)
-        # this prints the correct format but would prefer not to hard code the rjust() values
-        print(transposed_list[0].rjust(8), transposed_list[1].rjust(5), transposed_list[2].rjust(5))
+        transposed_list = list(i) # list constructor to i, y-axis of transposed table list, to new transposed list
+        #print(transposed_list)
+        #print(max(transposed_list, key=len))
+
+        print(transposed_list[0].rjust(8), transposed_list[1].rjust(5), transposed_list[2].rjust(5)) # use index to print transposed_listd
 
 table_data = [['apples', 'oranges', 'cherries', 'banana'],
               ['Alice', 'Bob', 'Carol', 'David'],
