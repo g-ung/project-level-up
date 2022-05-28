a9e309d9d3afb3ad7e79ec76b85cdf11bad6c99c
g-ung <45480999+g-ung@users.noreply.github.com>
2022-05-28 22:02:37 +1000

    Comment out print()

--- automate-the-boring-stuff-python/print_table.py
+++ automate-the-boring-stuff-python/print_table.py
@@ -22,7 +22,7 @@ def print_table(table):
         for string in table[count]: # search trough the string within the sublist of table
             if col_widths[count] < len(string):
                 col_widths[count] = len(string)
-                print(col_widths[count])
+                # print(col_widths[count])
         count += 1
 
     # transpose the nested list and print the table with rjust() proportionately to the longest string
@@ -38,6 +38,7 @@ def print_table(table):
 
         print(transposed_list[0].rjust(8), transposed_list[1].rjust(5), transposed_list[2].rjust(5)) # use index to print each columm transposed_list
     '''
+    
 table_data = [['apples', 'oranges', 'cherries', 'banana'],
               ['Alice', 'Bob', 'Carol', 'David'],
               ['dogs', 'cats', 'moose', 'goose']]
