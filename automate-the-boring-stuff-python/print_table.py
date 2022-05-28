b553619eac729338275c0eeb99f3d5f5b788a9c7
g-ung <45480999+g-ung@users.noreply.github.com>
2022-05-28 21:57:57 +1000

    Longest string and transposed list code
    Added searched for longest string code and refactored transposed list code with nested for loop.
    
    This method allows me to use col_widths[x] value to sub into rjust()

--- automate-the-boring-stuff-python/print_table.py
+++ automate-the-boring-stuff-python/print_table.py
@@ -13,14 +13,31 @@ cherries Carol moose
 NOTE: Assume that all the innner lists will contain the same number of strings
 '''
 def print_table(table):
-    # transpose the nested list and print the table
+    # create a list containing the same number of 0 values as the number of innter lists in table
+    col_widths = [0] * len(table) 
+
+    # find the longest string in nested list of table and store in col_widths
+    count = 0
+    while count < len(table): # iterate through nest listed in table
+        for string in table[count]: # search trough the string within the sublist of table
+            if col_widths[count] < len(string):
+                col_widths[count] = len(string)
+                print(col_widths[count])
+        count += 1
+
+    # transpose the nested list and print the table with rjust() proportionately to the longest string
+    for word in range(len(table[0])):
+        for list in range(len(table)):
+            print(table[list][word].rjust(col_widths[list]), end=' ')
+        print()
+
+    '''
     for i in zip(*table):
         transposed_list = list(i) # list constructor to i, y-axis of transposed table list, to new transposed list
         #print(transposed_list)
-        #print(max(transposed_list, key=len))
 
         print(transposed_list[0].rjust(8), transposed_list[1].rjust(5), transposed_list[2].rjust(5)) # use index to print each columm transposed_list
-
+    '''
 table_data = [['apples', 'oranges', 'cherries', 'banana'],
               ['Alice', 'Bob', 'Carol', 'David'],
               ['dogs', 'cats', 'moose', 'goose']]
