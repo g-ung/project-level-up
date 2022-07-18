c05cb433b4256c34024ed3bc34d4d93ad266e3ea
g-ung <45480999+g-ung@users.noreply.github.com>
2022-07-18 12:09:44 +1000

    Add code for OZ phone number 1x8

--- phone_and_email_extractor.py
+++ phone_and_email_extractor.py
@@ -10,10 +10,10 @@ How to Use this Script
 2. Copy the text, Linux\macOS: COMMAND-C (Win: CTRL-C)
 3. Run the script phone_and_email.py
 '''
-import pyperclip, re
+import re, pyperclip
 
 def main():
-    # phone number regex
+    # phone number regex, 2x4 digits
     phone_regex = re.compile(r'''(
         (\W\d{2})?                      # country code, optional
         (\s|-|\.)?                      # separator
@@ -22,6 +22,17 @@ def main():
         (\d{4})                         # first 4 digits
         (\s|-|\.)                       # seperator
         (\d{4})                         # last 4 digits
+        (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension, between two and five digits, optional
+        )''', re.VERBOSE)
+
+    # phone number regex, 1x8 digits
+    phone_nospace_regex = re.compile(r'''(
+        (\W\d{2})?                      # country code, optional
+        (\s|-|\.)?                      # separator
+        (\d{1,2}|\(\d{1,2}\))?          # area code, eg. 03 or (03), optional
+        (\s|-|\.)?                      # separator
+        (\d{8})                         # phone number with no spaces
+        (\s|-|\.)                       # separator
         (\s*(ext|x|ext.)\s*(\d{2,5}))?  # extension, between two and five digits, optional
         )''', re.VERBOSE)
 
@@ -38,11 +49,18 @@ def main():
     matches = [] # empty list to store mataches
     
     for groups in phone_regex.findall((text)): # use findall() to return a phone numbers and email address in a list of tuples; one tuple for each match
-        phone_num = ' '.join([groups[1], groups[3], groups[5], groups[7], groups[8]]) # groups are country code, area code, first four digits, last four digits
+        phone_num = '-'.join([groups[1], groups[3], groups[5], groups[7], groups[8]]) # groups are country code, area code, first four digits, last four digits
         if groups[9] != '':
             phone_num += ' x' + groups[9]
         matches.append(groups[0])
-    for groups in email_regex.findall(text):
+
+    for group in phone_nospace_regex.findall(text): # find phone numbers with no paces
+        phone_numnospace = '-'.join([group[1], group[3], group[4], group[7]]) # groups are country code, area code, block of 8 digits (phone no. with no spaces)
+        if group[8] != '':
+            phone_numnospace += ' x' + group[8]
+        matches.append(group[0])
+    
+    for groups in email_regex.findall(text): # find and append emails to matches list
         matches.append(groups[0])
 
     # copy the results to the clipboard
