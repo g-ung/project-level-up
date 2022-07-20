89061c5fc783242e510b1f85678bb42fd2bc8683
g-ung <45480999+g-ung@users.noreply.github.com>
2022-07-20 16:07:00 +1000

    Initial test case works

--- convert_ISO_date_to_dd-mm-yyyy_regex.py
+++ convert_ISO_date_to_dd-mm-yyyy_regex.py
@@ -16,23 +16,24 @@ def main():
         iso_date_regex = re.compile(r'''(
             (\d{4})                     # year in YYYY format
             (\.|-|\/)                   # separator
-            (0?[1-9]|1[0-2])             # month in MM format
+            (0?[1-9]|1[0-2])            # month in MM format
             (\.|-|\/)                   # separator
-            (0?[1-9]|[12][0-9]|3[01])    # day in DD format
+            (0?[1-9]|[12][0-9]|3[01])   # day in DD format
             )''', re.VERBOSE)
         
         # ISO basic date format regex
         iso_date_basic = re.compile(r'''(
-            (\d{4})                     # year in YYYY format
+            (\d{4})                      # year in YYYY format
             (0?[1-9]|1[0-2])             # month in MM format
             (0?[1-9]|[12][0-9]|3[01])    # day in DD format
             )''', re.VERBOSE)
 
         # regex test
-        string = "This is an iso date: 2022/07/19, 2022-07-20, 2022/7/3"
-
-        result = re.sub(iso_date_regex, '\\3-\\2-\\1', string, re.VERBOSE)
-        print(result)
+        mo1 = iso_date_regex.findall("This is an iso date variants: 2022/07/19, 2022-07-20, 2022/7/3, 20220808, 202296")
+        print(mo1)
+        
+        mo2 = iso_date_basic.findall("This is an iso date variants: 2022/07/19, 2022-07-20, 2022/7/3, 20220808, 202296")
+        print(mo2)
 
 
 if __name__ == '__main__':
