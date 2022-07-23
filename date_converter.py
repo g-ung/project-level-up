311eb751c7984beeb43524a14d342192bb5627b7
g-ung <45480999+g-ung@users.noreply.github.com>
2022-07-23 23:04:47 +1000

    Add code for pattern match
    WIP, it kinda works

--- date_converter.py
+++ date_converter.py
@@ -7,59 +7,68 @@ ISO 8601/US date pattern, then converts them to
 DD/MM/YYYY or DD/MM/YY format
 '''
 
-import re, fileinput
+import re
 from datetime import datetime
 
-def main():
-    filename = "date.txt"
+def lookup(dates):
+    # define regex patterns
+    # ISO date regex
+    iso_date_regex = re.compile(r'''(
+        (\d{4})                    # year in YYYY format
+        (\.|-|\/)                  # separator
+        (0[1-9]|1[0-2])            # month in MM format
+        (\.|-|\/)                  # separator
+        (0[1-9]|[12][0-9]|3[01])   # day in DD format
+        )''', re.VERBOSE)
+        
+    # ISO basic date format regex
+    iso_date_basic = re.compile(r'''(
+        (\d{4})                      # year in YYYY format
+        (0[1-9]|1[0-2])             # month in MM format
+        (0[1-9]|[12][0-9]|3[01])    # day in DD format
+        )''', re.VERBOSE)
 
+    # US date regex
+    us_date = re.compile(r'''(
+        (0[1-9]|1[0-2])            # month in MMformat
+        (\.|-|\/)                  # separator
+        (0[1-9]|[12][0-9]|3[01])    # day in DD format
+        (\.|-|\/)                  # separator
+        (\d{2,4})                  # year in YY or YYYY format
+        )''', re.VERBOSE)    
+    
+    desired_dt_format = '%d/%m/%Y'
+
+    patterns = {'%Y/%m/%d': iso_date_regex,
+                '%Y-%m-%d': iso_date_regex,
+                '%Y%m%d': iso_date_basic,
+                '%m/%d/%Y': us_date,
+                '%m-%d-%Y': us_date,
+                '%m/%d/%y': us_date,
+                '%m-%d-%y': us_date
+                }
+    
+    #filepath = "/Users/gabe_ung/Desktop/date.txt"
     # open a file, use findall() to find matches
-    with fileinput.FileInput(filename, inplace=True, backup='.bak') as f:
+    #with fileinput.FileInput(filepath, inplace=True, backup='.bak') as f:
+    try:
+        for dt_format, pattern in patterns.items():
+            if re.match(pattern, dates):
+                return "CONVERTED", datetime.strptime(dates, dt_format).strftime(desired_dt_format)
+    except Exception as e:
+        return "EXCEPTION", datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
+    return "NOT CONVERTED", datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
+   
+def main():
+    # place holder for .txt/.CSV file
+    test_dates = ['2022/07/18', '2022-07-18', '2022/7/18', '2022.7.18', '20220803', '20210603','202292', '2022903', '2022-07-21T02:10:46Z', '07/01/22', '08/01/2022', '08-21-21']
 
-        # ISO date regex
-        iso_date_regex = re.compile(r'''(
-            (\d{4})                    # year in YYYY format
-            (\.|-|\/)                  # separator
-            (0[1-9]|1[0-2])            # month in MM format
-            (\.|-|\/)                  # separator
-            (0[1-9]|[12][0-9]|3[01])   # day in DD format
-            )''', re.VERBOSE)
-        
-        # ISO basic date format regex
-        iso_date_basic = re.compile(r'''(
-            (\d{4})                      # year in YYYY format
-            (0?[1-9]|1[0-2])             # month in MM format
-            (0?[1-9]|[12][0-9]|3[01])    # day in DD format
-            )''', re.VERBOSE)
-
-        # US date regex
-        us_date = re.compile(r'''(
-            (0[1-9]|1[0-2])            # month in MMformat
-            (\.|-|\/)                  # separator
-            (0[1-9]|[12][0-9]|[01])    # day in DD format
-            (\.|-|\/)                  # separator
-            (\d{2,4})                  # year in YY or YYYY format
-            )''', re.VERBOSE)
-        
-        for line in f:
-            line = line.strip() # data clean up by removing leadding and trailing chars.
-            date_obj = None
-            if re.findall(iso_date_regex, line):
-                date_obj = datetime.strptime(line, '%Y-%m-%d') # why is %d is a different colour than the rest of the format?
-
-
-            
-        # regex test
-        #mo1 = iso_date_regex.findall("This is an iso date variants: 2022/07/19, 2022-07-20, 2022/7/3, 20220808, 202296")
-        #print(mo1)
-        #mo2 = iso_date_basic.findall("This is an iso date variants: 2022/07/19, 2022-07-20, 2022/7/3, 20220808, 202296")
-        #print(mo2)
-
-        #mo3 = iso_date_regex.findall(content)
-        #print(mo3)
-        #mo4 = iso_date_basic.findall(content)
-        #print(mo4)
-
+    for date in test_dates:
+        status, date_obj = lookup(date)
+        print(f'Input Date: [{date}] [{status}] to [{date_obj}]')
+    
+    #dt = datetime.strptime('2022/12/21', '%Y/%m/%d').strftime(desired_dt_format)
+    #print(dt)
 
 if __name__ == '__main__':
     main()
