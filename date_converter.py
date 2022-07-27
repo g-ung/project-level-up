eff61b91c8b59f1ecfc70b471afb1dedf98f483d
g-ung <45480999+g-ung@users.noreply.github.com>
2022-07-27 13:08:48 +1000

    Renamed lookup function to date_convert

--- date_converter.py
+++ date_converter.py
@@ -10,7 +10,7 @@ DD/MM/YYYY or DD/MM/YY format
 import re, fileinput
 from datetime import datetime
 
-def lookup(dates):
+def date_convert(filename):
 
     patterns = {
             '%Y/%m/%d': r'^\s*\d{4}\/\d{1,2}\/\d{1,2}\s*$',
@@ -23,25 +23,26 @@ def lookup(dates):
             }
     desired_dt_format = '%d/%m/%Y'
     
-    # test
-    #filepath = "/Users/gabe_ung/Desktop/date.txt"
-    # open a file, use findall() to find matches
-    #with fileinput.FileInput(filepath, inplace=True, backup='.bak') as f:
     try:
         for dt_format, pattern in patterns.items():
-            if re.findall(pattern, dates):
-                return "CONVERTED", datetime.strptime(dates, dt_format).strftime(desired_dt_format)
+            if re.match(pattern, filename):
+                return "CONVERTED", datetime.strptime(filename, dt_format).strftime(desired_dt_format)
     except Exception as e:
         return "EXCEPTION", datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
     return "NOT CONVERTED", datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
    
 def main():
     # place holder for .txt/.CSV file
-    test_dates = ['2022/07/18', '2022-07-19', '2022/7/18', '2022.7.18', '20220803', '20210603', '202292', '2022903', '2022-07-21T02:10:46Z', '07/01/22', '08/01/2022', '08-21-2021']
+    #test_dates = ['2022/07/18', '2022-07-19', '2022/7/18', '2022.7.18', '20220803', '20210603', '202292', '2022903', '2022-07-21T02:10:46Z', '07/01/22', '08/01/2022', '08-21-2021']
 
-    for date in test_dates:
-        status, date_obj = lookup(date)
-        print("Input Date: [{}] [{}] to [{}]".format(date, status, date_obj))
+    input_file = "/Users/gabe_ung/Desktop/date.txt"
+    # open a file, use findall() to find matches
+    #with fileinput.FileInput(input_file, inplace=True, backup='.bak') as f:
+
+
+    for line in input_file:
+        status, date_obj = date_convert(line)
+        print("Input Date: [{}] [{}] to [{}]".format(line, status, date_obj))
 
     #dt = datetime.strptime('2022/12/21', '%Y/%m/%d').strftime(desired_dt_format)
     #print(dt)
