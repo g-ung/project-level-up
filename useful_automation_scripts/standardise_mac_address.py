ffc3e8f3bc661ee4937a2c4b0e362f395aef575e
g-ung <45480999+g-ung@users.noreply.github.com>
2022-07-27 14:52:10 +1000

    Refactored code with list comprehension

--- useful_automation_scripts/standardise_mac_address.py
+++ useful_automation_scripts/standardise_mac_address.py
@@ -19,23 +19,14 @@ def main():
 
     # convert mac addresses to desired format e.g. MM-MM-MM-SS-SS-SS to MM:MM:MM:SS:SS:SS
     # replace "-" with ":"
-    mac_no_dash = [] # empty list to store new mac addresses with dash removed
-    for mac in mac_address:
-        mac_colon = mac.replace('-', ':') # edit this to suit your desired mac address format
-        mac_no_dash.append(mac_colon)
-    
+    mac_no_dash = [mac.replace('-', ':') for mac in mac_address] # edit this to suit your desired mac address format
+  
     # remove new line ("\n")
-    mac_remove_newline = [] # empty list to store new mac with "\n" removed
-    for mac in mac_no_dash:
-        mac_no_line = mac.replace("\n", "")
-        mac_remove_newline.append(mac_no_line)
+    mac_remove_newline = [mac.replace('\n', '') for mac in mac_no_dash]
     
     # convert capital to lowercase
-    desired_mac_format = [] # empty list to store the desire mac address format
-    for mac in mac_remove_newline:
-        mac_lowercase = mac.lower() # convert to lowercase, edit this to suit your desired mac address format
-        desired_mac_format.append(mac_lowercase)
-
+    desired_mac_format = [mac.lower() for mac in mac_remove_newline] # convert to lowercase, edit this to suit your desired mac address format
+    
     # write new mac address format to new text file
     # if output file hasn't already been created a new output file will be created
     output_file = "mac_address_standardise.txt"
