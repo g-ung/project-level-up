8a312ccea7a0c418bc3edae067e6aafd46c3fc0c
g-ung <45480999+g-ung@users.noreply.github.com>
2022-07-27 14:18:55 +1000

    Fixed typos

--- standardise_mac_address.py
+++ standardise_mac_address.py
@@ -1,22 +1,23 @@
 #! python 3
 '''
 Convert Mac Address Format
-Convert mac address wtih dashes ("-") and capital letters
-to lower case letters and colon (":").  
-This scrip will read from from an input text file, use for 
-loop to process the mac address from current format to desired
+Convert current mac address format (":", "-" or ".") and capital 
+(lowercase) letters to desired format.  
+
+This script will read from from an input text file, use for 
+loops to process the mac address from current format to desired
 mac address format
 '''
 def main():
     mac_address = [] # empty list to store mac addresses from file
 
     # read mac addresses from input file
-    input_file = "/Users/gabe_ung/Desktop/mac_addresses.txt" # enter the path to your file here
+    input_file = "mac_addresses.txt" # enter the path to your file here
     with open(input_file, 'r') as in_f:
         for line in in_f.readlines():
             mac_address.append(line)
 
-    # convert mac addresses to desired format, MM-MM-MM-SS-SS-SS to MM:MM:MM:SS:SS:SS
+    # convert mac addresses to desired format e.g. MM-MM-MM-SS-SS-SS to MM:MM:MM:SS:SS:SS
     # replace "-" with ":"
     mac_no_dash = [] # empty list to store new mac addresses with dash removed
     for mac in mac_address:
@@ -24,7 +25,7 @@ def main():
         mac_no_dash.append(mac_colon)
     
     # remove new line ("\n")
-    mac_remove_newline = [] # empty list to stor new mac with "\n" removed
+    mac_remove_newline = [] # empty list to store new mac with "\n" removed
     for mac in mac_no_dash:
         mac_no_line = mac.replace("\n", "")
         mac_remove_newline.append(mac_no_line)
@@ -32,11 +33,12 @@ def main():
     # convert capital to lowercase
     desired_mac_format = [] # empty list to store the desire mac address format
     for mac in mac_remove_newline:
-        mac_lowercase = mac.lower() # conver to lowercase, edit this to suit your desired mac address format
+        mac_lowercase = mac.lower() # convert to lowercase, edit this to suit your desired mac address format
         desired_mac_format.append(mac_lowercase)
 
     # write new mac address format to new text file
-    output_file = "/Users/gabe_ung/Desktop/mac_address_standardise.txt"
+    # if output file hasn't already been created a new output file will be created
+    output_file = "mac_address_standardise.txt"
     with open(output_file, 'w') as out_f:
         for i in desired_mac_format:
             out_f.write(i)
