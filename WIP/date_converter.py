1152068569290ea2f83eb7a9eff19346857d1f29
g-ung <45480999+g-ung@users.noreply.github.com>
2022-07-29 20:41:12 +1000

    Removed date converter function

--- WIP/date_converter.py
+++ WIP/date_converter.py
@@ -10,42 +10,98 @@ DD/MM/YYYY or DD/MM/YY format
 import re, fileinput
 from datetime import datetime
 
-def date_convert(filename):
 
-    patterns = {
-            '%Y/%m/%d': r'^\s*\d{4}\/\d{1,2}\/\d{1,2}\s*$',
-            '%m/%d/%Y': r'^\s*\d{1,2}\/\d{1,2}\/\d{4}\s*$',
-            '%Y-%m-%d': r'^\s*\d{2,4}-\d{1,2}-\d{1,2}\s*$',
-            '%m-%d-%Y': r'^\s*\d{1,2}-\d{1,2}-\d{4}\s*$',
-            '%m-%d-%y': r'^\s*\d{1,2}-\d{1,2}-\d{2}\s*$',
-            '%m/%d/%y': r'^\s*\d{1,2}\/\d{1,2}\/\d{2}\s*$',
-            '%Y%m%d': r'^\s*\d{4}\d{1,2}\d{1,2}\s*$'
-            }
-    desired_dt_format = '%d/%m/%Y'
-    
-    try:
-        for dt_format, pattern in patterns.items():
-            if re.match(pattern, filename):
-                return "CONVERTED", datetime.strptime(filename, dt_format).strftime(desired_dt_format)
-    except Exception as e:
-        return "EXCEPTION", datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
-    return "NOT CONVERTED", datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
-   
 def main():
     # place holder for .txt/.CSV file
     #test_dates = ['2022/07/18', '2022-07-19', '2022/7/18', '2022.7.18', '20220803', '20210603', '202292', '2022903', '2022-07-21T02:10:46Z', '07/01/22', '08/01/2022', '08-21-2021']
+    iso_dt_slash = re.compile(r'^\s*\d{4}\/\d{1,2}\/\d{1,2}\s*$')
+    iso_dt_dash = re.compile(r'^\s*\d{2,4}-\d{1,2}-\d{1,2}\s*$')
+    iso_dt_basic = re.compile(r'^\s*\d{4}\d{1,2}\d{1,2}\s*$')
+    us_dt_slash = re.compile(r'^\s*\d{1,2}\/\d{1,2}\/\d{4}\s*$')
+    us_dt_dash = re.compile(r'^\s*\d{1,2}-\d{1,2}-\d{4}\s*$')
+    us_dt_slash_yy = re.compile(r'^\s*\d{1,2}\/\d{1,2}\/\d{2}\s*$')
+    us_dt_dash_yy = re.compile(r'^\s*\d{1,2}\/\d{1,2}\/\d{2}\s*$')
 
-    input_file = "/Users/gabe_ung/Desktop/date.txt"
-    # open a file, use findall() to find matches
-    #with fileinput.FileInput(input_file, inplace=True, backup='.bak') as f:
+    desired_dt_format = '%d/%m/%Y'
+
+    patterns = {
+            '%Y/%m/%d': iso_dt_slash,
+            '%Y-%m-%d': iso_dt_dash,
+            '%m/%d/%Y': us_dt_slash,
+            '%m-%d-%Y': us_dt_dash,
+            '%m-%d-%y': us_dt_slash_yy,
+            '%m/%d/%y': us_dt_dash_yy,
+            '%Y%m%d': iso_dt_basic
+            }
+    dates = [] # empty list to store dates read from file
+    input_file = "/Users/gabe_ung/Desktop/dates.txt"
+    output_file = "/Users/gabe_ung/Desktop/converted_dates.txt"
+    with open(input_file, 'r') as in_f, open(output_file, 'w') as out_f:
+        for line in in_f.readlines():
+            dates.append(line.strip())
+        
+        
+        iso_dt_slash = re.compile(r'^\s*\d{4}\/\d{1,2}\/\d{1,2}\s*$')
+        iso_dt_dash = re.compile(r'^\s*\d{2,4}-\d{1,2}-\d{1,2}\s*$')
+        iso_dt_basic = re.compile(r'^\s*\d{4}\d{1,2}\d{1,2}\s*$')
+        us_dt_slash = re.compile(r'^\s*\d{1,2}\/\d{1,2}\/\d{4}\s*$')
+        us_dt_dash = re.compile(r'^\s*\d{1,2}-\d{1,2}-\d{4}\s*$')
+        us_dt_slash_yy = re.compile(r'^\s*\d{1,2}\/\d{1,2}\/\d{2}\s*$')
+        us_dt_dash_yy = re.compile(r'^\s*\d{1,2}\/\d{1,2}\/\d{2}\s*$')
+
+        desired_dt_format = '%d/%m/%Y'
+
+        patterns = {
+                '%Y/%m/%d': iso_dt_slash,
+                '%Y-%m-%d': iso_dt_dash,
+                '%m/%d/%Y': us_dt_slash,
+                '%m-%d-%Y': us_dt_dash,
+                '%m-%d-%y': us_dt_slash_yy,
+                '%m/%d/%y': us_dt_dash_yy,
+                '%Y%m%d': iso_dt_basic
+                }
+        for dt_format, pattern in patterns.items():
+            dt_obj = None
+            if re.match(pattern, dates):                
+                dt_obj = datetime.strptime(dates, dt_format)
+        
+        out_f.write(dt_obj.strftime(desired_dt_format) + '\n')
+    
 
 
-    for line in input_file:
-        status, date_obj = date_convert(line)
-        print("Input Date: [{}] [{}] to [{}]".format(line, status, date_obj))
 
+
+'''
+def date_convert(input):
+    iso_dt_slash = re.compile(r'^\s*\d{4}\/\d{1,2}\/\d{1,2}\s*$')
+    iso_dt_dash = re.compile(r'^\s*\d{2,4}-\d{1,2}-\d{1,2}\s*$')
+    iso_dt_basic = re.compile(r'^\s*\d{4}\d{1,2}\d{1,2}\s*$')
+    us_dt_slash = re.compile(r'^\s*\d{1,2}\/\d{1,2}\/\d{4}\s*$')
+    us_dt_dash = re.compile(r'^\s*\d{1,2}-\d{1,2}-\d{4}\s*$')
+    us_dt_slash_yy = re.compile(r'^\s*\d{1,2}\/\d{1,2}\/\d{2}\s*$')
+    us_dt_dash_yy = re.compile(r'^\s*\d{1,2}\/\d{1,2}\/\d{2}\s*$')
+
+    desired_dt_format = '%d/%m/%Y'
+
+    patterns = {
+            '%Y/%m/%d': iso_dt_slash,
+            '%Y-%m-%d': iso_dt_dash,
+            '%m/%d/%Y': us_dt_slash,
+            '%m-%d-%Y': us_dt_dash,
+            '%m-%d-%y': us_dt_slash_yy,
+            '%m/%d/%y': us_dt_dash_yy,
+            '%Y%m%d': iso_dt_basic
+            }
+    for dt_format, pattern in patterns.items():
+        try:
+            if re.match(pattern, input):                
+                return "CONVERTED", datetime.strptime(input, dt_format).strftime(desired_dt_format)
+        except Exception as e:
+            return "EXCEPTION", datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
+        return "NOT CONVERTED", datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
+'''
+    
     #dt = datetime.strptime('2022/12/21', '%Y/%m/%d').strftime(desired_dt_format)
     #print(dt)
-
 if __name__ == '__main__':
     main()
--- useful_automation_scripts/standardise_mac_address.py
+++ useful_automation_scripts/standardise_mac_address.py
@@ -1,15 +1,15 @@
 #! python 3
 '''
 Convert Mac Address Format
-This script will read from an input text file, use for 
-loops to process the mac address from current format to desired
-mac address format
+This script will read mac addresses from an input text file, use 
+list comprehensions to process the mac address from current format 
+to desired mac address format.
 '''
 def main():
     mac_address = [] # empty list to store mac addresses from file
 
     # read mac addresses from input file
-    input_file = "Users/gabe_ung/Desktop/mac_addresses.txt" # enter the path to your file here
+    input_file = "mac_addresses.txt" # enter the path to your file here
     with open(input_file, 'r') as in_f:
         for line in in_f.readlines():
             mac_address.append(line.strip())
@@ -18,15 +18,12 @@ def main():
     # replace "-" with ":"
     mac_no_dash = [mac.replace('-', ':') for mac in mac_address] # edit this to suit your desired mac address format
   
-    # remove new line ("\n")
-    #mac_remove_newline = [mac.replace('\n', '') for mac in mac_no_dash]
-    
     # convert capital to lowercase
     desired_mac_format = [mac.lower() for mac in mac_no_dash] # convert to lowercase, edit this to suit your desired mac address format
     
     # write new mac address format to new text file
     # if output file hasn't already been created a new output file will be created
-    output_file = "Users/gabe_ung/Desktop/mac_address_standardise.txt"
+    output_file = "mac_address_standardise.txt"
     with open(output_file, 'w') as out_f:
         for i in desired_mac_format:
             out_f.write(i)
