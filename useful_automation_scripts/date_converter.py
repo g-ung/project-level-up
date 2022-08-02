1522851049ec3008be866382808f63ed55bff514
g-ung <45480999+g-ung@users.noreply.github.com>
2022-08-02 15:29:21 +1000

    Add feature date_converter function

--- useful_automation_scripts/date_converter.py
+++ useful_automation_scripts/date_converter.py
@@ -10,7 +10,7 @@ DD/MM/YYYY or DD/MM/YY format.
 import re
 from datetime import datetime
 
-def main():
+def date_converter(input_date):
     # date regex
     iso_dt_slash = re.compile(r'^\s*\d{4}\/\d{1,2}\/\d{1,2}\s*$')
     iso_dt_dash = re.compile(r'^\s*\d{4}-\d{1,2}-\d{1,2}\s*$')
@@ -21,38 +21,38 @@ def main():
     us_dt_dash_yy = re.compile(r'^\s*\d{1,2}-\d{1,2}-\d{2}\s*$')
 
     desired_dt_format = '%d/%m/%Y'
-    '''
-    patterns = {
-            '%Y/%m/%d': iso_dt_slash,
-            '%Y-%m-%d': iso_dt_dash,
-            '%m/%d/%Y': us_dt_slash,
-            '%m-%d-%Y': us_dt_dash,
-            '%m-%d-%y': us_dt_slash_yy,
-            '%m/%d/%y': us_dt_dash_yy,
-            '%Y%m%d': iso_dt_basic
-            }
-    '''
-    # date match and write to output file
-    input_file = "dates.txt"
+    
     output_file = "converted_dates.txt"
-    with open(input_file, 'r') as in_f, open(output_file, 'w') as out_f:
+    with open(output_file, 'w') as out_f:
+        if re.match(iso_dt_slash, input_date):
+            dt_obj = datetime.strptime(input_date, '%Y/%m/%d')
+        elif re.match(iso_dt_dash, input_date):
+            dt_obj = datetime.strptime(input_date, '%Y-%m-%d')
+        elif re.match(us_dt_slash, input_date):
+            dt_obj = datetime.strptime(input_date, '%m/%d/%Y')
+        elif re.match(us_dt_dash, input_date):
+            dt_obj = datetime.strptime(input_date, '%m-%d-%Y')
+        elif re.match(us_dt_slash_yy,input_date):
+            dt_obj = datetime.strptime(input_date, '%m/%d/%y')
+        elif re.match(us_dt_dash_yy, input_date):
+            dt_obj = datetime.strptime(input_date, '%m-%d-%y')
+        elif re.match(iso_dt_basic, input_date):
+            dt_obj = datetime.strptime(input_date, '%Y%m%d')
+        out_f.write(dt_obj.strftime(desired_dt_format) + '\n')
+        '''
+            return 'CONVERTED', dt_obj.strftime(desired_dt_format)
+        except Exception as e:
+            return 'EXCEPTION', datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
+        return 'NOT CONVERTED', datetime.now().replace(microsecond=0, second=0, minute=0, hour=0)
+        '''
+def main():
+    # date match and write to output file
+    input_file = "/Users/gabe_ung/Desktop/dates.txt"
+    with open(input_file, 'r') as in_f: 
         for line in in_f.readlines():
             line = line.strip()
-            if re.match(iso_dt_slash, line):
-                dt_obj = datetime.strptime(line, '%Y/%m/%d')
-            elif re.match(iso_dt_dash, line):
-                dt_obj = datetime.strptime(line, '%Y-%m-%d')
-            elif re.match(us_dt_slash, line):
-                dt_obj = datetime.strptime(line, '%m/%d/%Y')
-            elif re.match(us_dt_dash, line):
-                dt_obj = datetime.strptime(line, '%m-%d-%Y')
-            elif re.match(us_dt_slash_yy,line):
-                dt_obj = datetime.strptime(line, '%m/%d/%y')
-            elif re.match(us_dt_dash_yy, line):
-                dt_obj = datetime.strptime(line, '%m-%d-%y')
-            elif re.match(iso_dt_basic, line):
-                dt_obj = datetime.strptime(line, '%Y%m%d')
-            out_f.write(dt_obj.strftime(desired_dt_format) + '\n')
+            date_converter(line)
+            #print("Input Date: {}] [{}] to [{}]".format(line, status, date_obj))
 
 if __name__ == '__main__':
     main()
