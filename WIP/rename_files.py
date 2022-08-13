6574c51d6f6845e4c770a13afe7f83f731acc17b
g-ung <45480999+g-ung@users.noreply.github.com>
2022-08-13 13:04:00 +1000

    Add file

---
+++ WIP/rename_files.py
@@ -0,0 +1,22 @@
+#! python 3
+'''
+Reformat and Rename File Name
+
+'''
+import os
+
+def main():
+    os.chdir('enter file path to your files here')
+
+    for file in os.listdir():
+        file_name, file_ext = os.path.splitext(file)
+        file_title, file_body, file_num = file_name.split('_')
+        file_num = file_num.strip()[1:].zfill(2) # strip unwanted char, use zfill() to pad numnber with a zero
+        
+        #print("{}-{}-{}{}".format(file_num, file_title, file_body, file_ext))
+        new_name = '{}-{}-{}{}'.format(file_num, file_title, file_body, file_ext)
+
+        os.rename(file, new_name)
+
+if __name__ == '__main__':
+    main()
