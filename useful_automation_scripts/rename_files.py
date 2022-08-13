bf7a54d8532d065a286283201c1219964f6e5a1e
g-ung <45480999+g-ung@users.noreply.github.com>
2022-08-13 13:14:17 +1000

    Add additional comments

--- useful_automation_scripts/rename_files.py
+++ useful_automation_scripts/rename_files.py
@@ -1,6 +1,8 @@
 #! python 3
 '''
 Reformat and Rename File Name
+Edit the lines of code as required to reformat current
+file name to format of new file name
 
 '''
 import os
@@ -9,14 +11,25 @@ def main():
     os.chdir('enter file path to your files here')
 
     for file in os.listdir():
+        # check with
+        #print(file)
+        #print(os.path.splitext(file))
+        
+        # assign variable names to each tuple
         file_name, file_ext = os.path.splitext(file)
+        
+        # assign variable names to each section of file name at split('_')
         file_title, file_body, file_num = file_name.split('_')
-        file_num = file_num.strip()[1:].zfill(2) # strip unwanted char, use zfill() to pad numnber with a zero
+
+        # strip unwanted char, use zfill() to pad numnber with a zero
+        file_num = file_num.strip()[1:].zfill(2) 
         
         #print("{}-{}-{}{}".format(file_num, file_title, file_body, file_ext))
+        
+        # new name format
         new_name = '{}-{}-{}{}'.format(file_num, file_title, file_body, file_ext)
 
-        os.rename(file, new_name)
+        os.rename(file, new_name) # rename file according the name format, line 22
 
 if __name__ == '__main__':
     main()
