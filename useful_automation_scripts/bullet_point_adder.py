8295715c15582864c2242f538aedae6c6ab7c236
g-ung <45480999+g-ung@users.noreply.github.com>
2022-08-22 22:39:25 +1000

    Update bullet_point_adder.py

--- useful_automation_scripts/bullet_point_adder.py
+++ useful_automation_scripts/bullet_point_adder.py
@@ -8,17 +8,17 @@ of each line of text on the clipboard
 import pyperclip
 
 def main():
-text = pyperclip.paste() # variable to store list of text
+    text = pyperclip.paste() # variable to store list of text
 
-# Seperating list of text into lines and add asterix
-lines = text.split("\n") # use split() to seperate the text at '\n', newlines, to get the list in one line of text
-for i in range(len(lines)): # iterate thruough all indext in the 'lines' list
-    lines[i] = '* ' + lines[i] # for each line in the list add '*' to start of the text
+    # Seperating list of text into lines and add asterix
+    lines = text.split("\n") # use split() to seperate the text at '\n', newlines, to get the list in one line of text
+    for i in range(len(lines)): # iterate thruough all indext in the 'lines' list
+        lines[i] = '* ' + lines[i] # for each line in the list add '*' to start of the text
 
-# join the modifed lines
-text = '\n'.join(lines) # use join() to join the line  at '\n'
+    # join the modifed lines
+    text = '\n'.join(lines) # use join() to join the line  at '\n'
 
-pyperclip.copy(text)
+    pyperclip.copy(text)
 
 if __name__ == '__main__':
     main()
