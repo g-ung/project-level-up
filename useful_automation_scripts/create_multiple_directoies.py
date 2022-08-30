5c343e4ca36c73f141e731fcec16d2b30b835ada
g-ung <45480999+g-ung@users.noreply.github.com>
2022-08-30 15:15:18 +1000

    Add additional comment

--- useful_automation_scripts/create_multiple_directoies.py
+++ useful_automation_scripts/create_multiple_directoies.py
@@ -2,22 +2,26 @@
 '''
 Create Multiple New Diretories
 Edit the block of code inside the for loop
-as required
+as required.
+
+This script was created so I could create multple
+directories in sequence in one of the folders in my
+GitHub repository
 '''
 import os
 
 def main():
     # change to the diretory where you want to create your new directories
-    path = '/Users/gabe_ung/Desktop/Projects/project-level-up/automate-the-boring-stuff-with-python-SOLUTIONS'
+    path = '' # insert your path here
     os.chdir(path) 
    
-    # dircectory name/directory prefix, change this variabl as required
+    # dircectory name/directory prefix, change this variable as required
     directory = 'Chapter_'
     # check with
     #print(dir(os))
     #print('Current working directory: ', os.getcwd())
 
-    # use range to specify how many directories you need created
+    # use range to specify the number of directories to create
     count = 0
     for i in range(8, 21):
         os.mkdir(directory + str(i))
