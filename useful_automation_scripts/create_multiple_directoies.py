783f99ba630f1e83ccef493cba56851b472dceb6
g-ung <45480999+g-ung@users.noreply.github.com>
2022-08-30 17:05:54 +1000

    Add comments to functions

--- useful_automation_scripts/create_multiple_directoies.py
+++ useful_automation_scripts/create_multiple_directoies.py
@@ -1,33 +1,78 @@
 #! python 3
 '''
 Create Multiple New Diretories
-Edit the block of code inside the for loop
-as required.
-
-This script was created so I could create multple
-directories, in sequence, in one of the folders in my
-GitHub repository
+Insert the directory name, source file path, and path 
+(where the new directories are to be created) in the
+variables provided in main() as required.
 '''
 import os
 
-def main():
-    # change to the diretory where you want to create your new directories
-    path = '' # insert your path here
-    os.chdir(path) 
-   
-    # dircectory name/directory prefix, change this variable as required
-    directory = 'Chapter_'
-    # check with
-    #print(dir(os))
-    #print('Current working directory: ', os.getcwd())
+def new_dir_seq(dir_path, dir_name):
+    '''
+    Funtion to create multiple directories with sequencial names
+    e.g. dir_1, dir_2, dir_3 ... dir_n
 
-    # use range to specify the number of directories to create
+    Args:
+        dir_path (string): the directory path where you want your new directories created
+        dir_name (string): name of your directory: name, prefix or suffix
+
+    Returns:
+        print statment: status with number of directories created and where they have been created
+    '''
+    # change diretory where you want to create your new directories
+    # path = '' # insert your path here
+    os.chdir(dir_path) 
+   
+    # create new directories in sequence
+    # use range to specify the number of sequencial directories to create
     count = 0
-    for i in range(8, 21):
-        os.mkdir(directory + str(i))
+    for i in range(1, 4):
+        os.mkdir(dir_name + str(i)) # edit direcotry name sequence here
         count += 1
     
-    print('Directories created, {} directories created in {}'.format(count, path))
+    print('Directories created, {} directories created in {}'.format(count, dir_path))
+
+def new_dir_multiple(folder_path, file_path):
+    '''
+    Funtion to create multiple directories where name of directories are from a source file
+
+    Args:
+        folder_path (string): the directory path where you want your new directories created
+        file_path (string): file path of your source file
+
+    Returns:
+        print statment: status with number of directories created and where they have been created
+    '''
+    # change to the diretory where you want to create your new directories
+    # path = '' # insert your path here
+    os.chdir(folder_path) 
+
+    # create multiple directories where name of directories are from a source file
+    # input_file = ''# insert location of your input file here
+    count = 0
+    with open(file_path, 'r') as f:
+        for dir in f.readlines():
+            dir = dir.strip()
+            os.mkdir(dir)
+            count += 1
+    
+    print('Directories created, {} directories created in {}'.format(count, file_path))
+
+def main():
+    # dircectory name/prefix/suffix, change this variable as required
+    directory = 'Chapter_'
+    
+    # directory path where you want the new directories to be created
+    path = ''
+
+    # file path of your source file
+    input_file = ''
+
+    # create multple new directories witih sequencial names
+    new_dir_seq(path, directory)
+
+    # create multple new directories with different names from source file
+    new_dir_multiple(path, input_file)
 
 if __name__ == '__main__':
     main()
